3205bf2ff8bd5e3aa21a4728787589e96188eb86
akaamosdev <amosaka55@gmail.com>
2023-01-21 12:25:58 +0000

    add-project-01

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/list-participant', function () {
+    return view('liste-participant');
+});
